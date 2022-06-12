4983573df3393cf9ea52e78cdf8ed6671c2ba13c
644709020018 <644708020018@dpu.ac.th>
2022-06-12 14:01:17 +0700

    LighSwitch_pop

---
+++ IT267-881/lighSwich_oop.py
@@ -0,0 +1,30 @@
+#LighSwitch_oop
+class Switch():
+    def __init__(self):
+        self.switch_status = False
+
+    def turnOn(self):
+        self.switch_status = True
+
+    def turnOff(self):
+        self.switch_status = False
+
+    def show_status(self):
+        if (self.switch_status):
+            print ("ไฟเปิด")
+        else:
+            print ("ไฟปิด")
+sobj = Switch()
+
+if __name__ == "__main__":
+    sobj.show_status()
+    sobj.turnOn()
+    sobj.show_status()
+    sobj.turnOff()
+    sobj.show_status()
+    sobj.turnOff()
+    sobj.show_status()
+    sobj.turnOff()
+    sobj.show_status()
+    sobj.turnOff()
+    sobj.show_status()
