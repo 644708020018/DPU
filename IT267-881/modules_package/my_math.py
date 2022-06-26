de65485f2ff657e8f68b1dbd7e57225ec6602b59
644709020018 <644708020018@dpu.ac.th>
2022-06-26 09:54:33 +0700

    sara

---
+++ IT267-881/modules_package/my_math.py
@@ -0,0 +1,10 @@
+#my_math.py
+PI = 3.1416
+
+def sqrt(number):
+    return (number **2)
+
+def circle_area(radius):
+    area = PI * radius * radius
+    return area
+
---
+++ IT267-881/modules_package/my_math_use.py
@@ -0,0 +1,12 @@
+import my_math
+
+#วิธีที่ 1
+#print (f' PI = {my_math.PI}')
+#print (f'sqrt of 5 = {my_math.sqrt(5)}')
+#print (f'circle_area = {my_math.circle_area(2):,.2f}')
+
+#วิธีที่ 2
+#from my_math import PI,sqrt,circle_area
+print (f' PI = {my_math.PI}')
+print (f'sqrt of 5 = {my_math.sqrt(5)}')
+print (f'circle_area = {my_math.circle_area(2):,.2f}')
